d23d55f0b3c48f1e0e471ba56180a9ec292be697
likeyou2 <1394083189@qq.com>
2019-05-21 07:02:28 +0000

    debug

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -378,7 +378,7 @@ class WeixinController extends Controller
     public function webAuth(){
         $webUrl = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
 	    $jump = urlencode($webUrl.'/webAuthDo');
-	    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('WX_APPID').'&redirect_uri='.$jump.'&response_type=code&scope=snsapi_base &state=STATE#wechat_redirect';
+	    $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.env('WX_APPID').'&redirect_uri='.$jump.'&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect';
         header("location:".$url);
     }
 
