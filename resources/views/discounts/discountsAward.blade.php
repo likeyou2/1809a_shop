25cc8fb6e0e4115fc9d6f48b3969cc9f5890d383
likeyou2 <1394083189@qq.com>
2019-05-31 02:11:57 +0000

    deBug

--- resources/views/discounts/discountsAward.blade.php
+++ resources/views/discounts/discountsAward.blade.php
@@ -1,1 +1,5 @@
-<input type="submit" value="点击抽奖" style="color:#2b2b2b;background-color:#0d6aad;width:150px;height:35px;font-size:20px;border:none;"><br>
+<form action="">
+    <div style="text-align:center;margin:auto;">
+        <input type="submit" value="点击抽奖" style="color:#2b2b2b;background-color:#0d6aad;width:150px;height:35px;font-size:20px;border:none;"><br>
+    </div>
+</form>
